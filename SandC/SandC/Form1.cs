fbb25336c14ac7b7ab73dff1a781b99e24fd98e7
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2018-12-02 14:27:25 +0000

    Form Template + IP Aquire
    Form Template has been madde aswell as a Text box which aquires the Host machines IP address for the Server

---
+++ SandC/SandC/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SandC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName()); // Aquires Host machines IP Address
+            foreach (IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ServerIPtextBox.Text = address.ToString();
+                }
+            }
+        }
+
+        private void StartServerbutton_Click(object sender, EventArgs e) // Start Server Functionality
+        {
+
+        }
+    }
+}
